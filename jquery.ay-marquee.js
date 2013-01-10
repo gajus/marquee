68e9db33e35c7257bb90578e21165a249d43af55
Gajus Kuizinas <g.kuizinas@anuary.com>
2013-01-10 00:58:24 +0000

    Improved logic allows arbitrary children width.

--- jquery.ay-marquee.js
+++ jquery.ay-marquee.js
@@ -11,9 +11,17 @@
 	'use strict';
 	
 	var nativeTransition;
-
-	$.fn.ayMarquee = function (options) {
-		var settings = $.extend({
+	
+	$.ay = $.ay || {};
+	
+	$.ay.marquee = function (options) {
+		var settings;
+	
+		if (!options.target || !options.target instanceof $) {
+			throw 'Target is not defined or it is not instance of jQuery.';
+		}
+	
+		settings = $.extend({
 			direction: 'left',
 			speed: 800
 		}, options);
@@ -29,7 +37,7 @@
 		
 		settings.direction	= settings.direction === 'left' ? 'left' : 'right';
         
-        var uniqueStyle = (function () {
+        /*var uniqueStyle = (function () {
 			var unique = {}; // @todo
 			
 			return function (childWidth) {
@@ -39,29 +47,79 @@
 				
 				return className;
 			};
-		}());
+		}());*/ 
 		
-		this.each(function(){
+		options.target.each(function () {
 			var container = $(this),
+				containerWidth = container.width(),
 				wrapper = container.children().eq(0),
 				children = wrapper.children(),
 				// assume that all children have identical width
-				childWidth = children.eq(0).outerWidth(true),
-				calculatedWrapperWidth = children.length*childWidth;
+				calculatedWrapperWidth = 0,
+				childNumber = 0;
+			
+			children.each(function () {
+				calculatedWrapperWidth += $(this).outerWidth(true);
+			});
 			
 			wrapper.width(calculatedWrapperWidth);
 			
-			var childNumber = 0;
+			var uniqueStyle = (function () {
+				var assignedClasses = {};
+				
+				return function (elementWidth) {
+					var duration,
+						className;
+				
+					if (elementWidth in assignedClasses) {
+						return assignedClasses[elementWidth];
+					}
+				
+					duration = (elementWidth/containerWidth)*settings.speed;
+					className = 'ay-transition-' + Math.random().toString(36).substr(2, 5);
+					
+					$('<style>.' + className + ' { ' + nativeTransition + ': margin-' + settings.direction + ' ' + duration + 'ms linear; margin-' + settings.direction + ': -' + elementWidth + 'px!important; }</style>').appendTo('head');
+					
+					return assignedClasses[elementWidth] = {name: className, duration: duration};
+				}
+			}());
 			
 			if (nativeTransition) {
+				var child;
+				var lastName;
+				
+				var animate = function animate() {
+					if (child) {
+						child.appendTo(wrapper).removeClass(lastName);
+					}
+					
+					child 	= children.eq(childNumber);
+					
+					var transition = uniqueStyle(child.outerWidth(true));
+					
+					console.log(transition);
+					
+					child.addClass( transition.name );
+					
+					lastName = transition.name;
+					
+					childNumber = children.length > childNumber+1 ? childNumber+1 : 0;
+					
+					setTimeout(animate, transition.duration)
+				}();
+			}
+			
+			
+			/*
+			
+			 if (nativeTransition) {
 				var uniqueTransitionName = uniqueStyle(childWidth);
 				
 				//uniqueTransitionName
 				var child;
 				
-				var animate = function animate(){
-					if(child)
-					{
+				var animate = function animate() {
+					if (child) {
 						child.appendTo(wrapper).removeClass(uniqueTransitionName);
 					}
 					
@@ -73,7 +131,7 @@
 					
 					setTimeout(animate, settings.speed)
 				}();
-			} else {
+			}else {
 				var margin_rule = function(margin){
 					var obj = {};
 				
@@ -82,12 +140,12 @@
 					return obj;
 				};
 			
-				var animate = function(){
+				var animate = function () {
 					var child = children.eq(childNumber);
 					
 					childNumber = children.length > childNumber+1 ? childNumber+1 : 0;
 					
-					child.animate(margin_rule(-childWidth), {duration: settings.speed, easing: 'linear', complete: function(){
+					child.animate(margin_rule(-childWidth), {duration: settings.speed, easing: 'linear', complete: function () {
 						child.appendTo(wrapper).css(margin_rule(0));
 						
 						animate();
@@ -95,7 +153,7 @@
 				};
 				
 				animate();
-			}			
+			}*/			
 		});
 	};
 })();
