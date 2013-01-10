4adcf32235a0551b8be8ad75be0f9b8945408b70
Gajus Kuizinas <g.kuizinas@anuary.com>
2013-01-10 15:02:31 +0000

    CSS2 and CSS3 implementation with scroll and transitions.

--- jquery.ay-marquee.js
+++ jquery.ay-marquee.js
@@ -7,11 +7,130 @@
  *
  * Author: Gajus Kuizinas <g.kuizinas@anuary.com>
  */
-(function(){
+$(function(){ // This plugin relies on DOM ready state. Do not move it to IIFE.
 	'use strict';
 	
-	var nativeTransition;
+	var nativeTransition,
+		marqueeImplementation,
+		css3Implementation,
+		cssImplementation;
 	
+	nativeTransition =
+		'WebkitTransition' in document.body.style && '-webkit-transition' ||
+		'MozTransition' in document.body.style && '-moz-transition' ||
+		'msTransition' in document.body.style && '-ms-transition' ||
+		'OTransition' in document.body.style && '-o-transition' ||
+		'Transition' in document.body.style && 'transition';
+    
+    marqueeImplementation = function (settings) {
+    	var container = $(this),
+			containerWidth = container.width(),
+			wrapper = container.children().eq(0),
+			wrapperContentWidth = 0,
+			children = wrapper.children(),
+			childrenTransitions = [],
+			childNumber = 0,
+			assignTransition;
+		
+		assignTransition = (function () {
+			var assigned = {};
+			
+			return function (elementWidth) {
+				var duration,
+					className;
+					
+				if (elementWidth in assigned) {
+					return assigned[elementWidth];
+				}
+				
+				duration = (elementWidth/containerWidth)*settings.speed;
+				
+				if (nativeTransition) {
+					className = 'ay-transition-' + Math.random().toString(36).substr(2, 10);
+						
+					$('<style>.' + className + ' { ' + nativeTransition + ': margin-' + settings.direction + ' ' + duration + 'ms linear; margin-' + settings.direction + ': -' + elementWidth + 'px!important; }</style>').appendTo('head');
+				}
+				
+				return assigned[elementWidth] = {name: className, elementWidth: elementWidth, duration: duration};
+			}
+		}());
+		
+		children.each(function () {
+			var width = $(this).outerWidth(true);
+			
+			childrenTransitions.push( assignTransition(width) );
+		
+			wrapperContentWidth += width;
+		});
+		
+		wrapper.width(wrapperContentWidth);
+		
+		if (settings.implementation === 'css3' && nativeTransition) {
+			container.addClass('ay-css3');
+		
+			(function animate(child, lastName) {
+				var transition;
+			
+				if (child) {
+					child.appendTo(wrapper).removeClass(lastName);
+				}
+				
+				child 	= children.eq(childNumber);
+				
+				transition = childrenTransitions[childNumber];
+				
+				child.addClass( transition.name );
+				
+				lastName = transition.name;
+				
+				childNumber = children.length > childNumber + 1 ? childNumber + 1 : 0;
+				
+				
+				
+				setTimeout(function () {
+					animate(child, lastName);
+				}, transition.duration)
+			}());
+		} else {
+			container.addClass('ay-css2');
+		
+			(function () {
+				// @todo merge the left/right implementations
+				if (settings.direction === 'left') {
+					(function animate() {
+						var child = children.eq(childNumber),
+							transition = childrenTransitions[childNumber];
+						
+						childNumber = children.length > childNumber + 1 ? childNumber + 1 : 0;
+						
+						container.animate({scrollLeft: transition.elementWidth}, {duration: transition.duration, easing: 'linear', complete: function () {
+							child.appendTo(wrapper);
+							container.scrollLeft(0);
+							
+							animate();
+						}});
+					}());
+				} else {
+					(function animate() {
+						var child = children.eq(children.length-1-childNumber),
+							transition = childrenTransitions[children.length-1-childNumber];
+						
+						childNumber = children.length > childNumber + 1 ? childNumber + 1 : 0;
+						
+						container.scrollLeft(wrapperContentWidth-containerWidth);
+						
+						container.animate({scrollLeft: wrapperContentWidth-containerWidth-transition.elementWidth}, {duration: transition.duration, easing: 'linear', complete: function () {
+							child.prependTo(wrapper);
+							container.scrollLeft(wrapperContentWidth-containerWidth);
+							
+							animate();
+						}});
+					}());
+				}
+			}());
+		}
+	};
+    	
 	$.ay = $.ay || {};
 	
 	$.ay.marquee = function (options) {
@@ -23,137 +142,14 @@
 	
 		settings = $.extend({
 			direction: 'left',
-			speed: 800
+			speed: 800,
+			implementation: 'css3'
 		}, options);
 		
-		if (nativeTransition === undefined) {
-			nativeTransition =
-				'WebkitTransition' in document.body.style ? '-webkit-transition' : false ||
-		        'MozTransition' in document.body.style ? '-moz-transition' : false ||
-		        'msTransition' in document.body.style ? '-ms-transition' : false ||
-		        'OTransition' in document.body.style ? '-o-transition' : false ||
-		        'Transition' in document.body.style ? 'transition' : false ;
-		}
-		
 		settings.direction	= settings.direction === 'left' ? 'left' : 'right';
-        
-        /*var uniqueStyle = (function () {
-			var unique = {}; // @todo
-			
-			return function (childWidth) {
-				var className = 'ay-transition-' + Math.random().toString(36).substr(2, 5);
-				
-				$('<style>.' + className + ' { ' + nativeTransition + ': margin-' + settings.direction + ' ' + settings.speed + 'ms linear; margin-' + settings.direction + ': -' + childWidth + 'px!important; }</style>').appendTo('head');
-				
-				return className;
-			};
-		}());*/ 
 		
 		options.target.each(function () {
-			var container = $(this),
-				containerWidth = container.width(),
-				wrapper = container.children().eq(0),
-				children = wrapper.children(),
-				// assume that all children have identical width
-				calculatedWrapperWidth = 0,
-				childNumber = 0;
-			
-			children.each(function () {
-				calculatedWrapperWidth += $(this).outerWidth(true);
-			});
-			
-			wrapper.width(calculatedWrapperWidth);
-			
-			var uniqueStyle = (function () {
-				var assignedClasses = {};
-				
-				return function (elementWidth) {
-					var duration,
-						className;
-				
-					if (elementWidth in assignedClasses) {
-						return assignedClasses[elementWidth];
-					}
-				
-					duration = (elementWidth/containerWidth)*settings.speed;
-					className = 'ay-transition-' + Math.random().toString(36).substr(2, 5);
-					
-					$('<style>.' + className + ' { ' + nativeTransition + ': margin-' + settings.direction + ' ' + duration + 'ms linear; margin-' + settings.direction + ': -' + elementWidth + 'px!important; }</style>').appendTo('head');
-					
-					return assignedClasses[elementWidth] = {name: className, duration: duration};
-				}
-			}());
-			
-			if (nativeTransition) {
-				var child;
-				var lastName;
-				
-				var animate = function animate() {
-					if (child) {
-						child.appendTo(wrapper).removeClass(lastName);
-					}
-					
-					child 	= children.eq(childNumber);
-					
-					var transition = uniqueStyle(child.outerWidth(true));
-					
-					console.log(transition);
-					
-					child.addClass( transition.name );
-					
-					lastName = transition.name;
-					
-					childNumber = children.length > childNumber+1 ? childNumber+1 : 0;
-					
-					setTimeout(animate, transition.duration)
-				}();
-			}
-			
-			
-			/*
-			
-			 if (nativeTransition) {
-				var uniqueTransitionName = uniqueStyle(childWidth);
-				
-				//uniqueTransitionName
-				var child;
-				
-				var animate = function animate() {
-					if (child) {
-						child.appendTo(wrapper).removeClass(uniqueTransitionName);
-					}
-					
-					child 	= children.eq(childNumber);
-					
-					child.addClass(uniqueTransitionName);
-					
-					childNumber = children.length > childNumber+1 ? childNumber+1 : 0;
-					
-					setTimeout(animate, settings.speed)
-				}();
-			}else {
-				var margin_rule = function(margin){
-					var obj = {};
-				
-					obj['margin-' + settings.direction] = margin;
-				
-					return obj;
-				};
-			
-				var animate = function () {
-					var child = children.eq(childNumber);
-					
-					childNumber = children.length > childNumber+1 ? childNumber+1 : 0;
-					
-					child.animate(margin_rule(-childWidth), {duration: settings.speed, easing: 'linear', complete: function () {
-						child.appendTo(wrapper).css(margin_rule(0));
-						
-						animate();
-					}});
-				};
-				
-				animate();
-			}*/			
+			marqueeImplementation.call(this, settings);
 		});
 	};
-})();
+});
